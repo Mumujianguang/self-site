cd0c0119834195b46cda66e803b7b1fcad6e2f3f
linjianguang <1419479392@qq.com>
2024-11-11 17:25:59 +0800

    test: release ci/cd

--- site-fe/src/pages/Home/index.tsx
+++ site-fe/src/pages/Home/index.tsx
@@ -7,8 +7,13 @@ import { router } from '@/router';
 
 import styles from './style.module.less'
 import Signature from '@/components/Signature';
+import { MY_LINKS } from '@/constants';
 
-export default function Home(props: any) {
+/**
+ * Home page
+ * @returns
+ */
+export default function Home() {
     const navigator = useNavigate()
     const [activeRoute, setActiveRoute ] = useState('menu');
 
@@ -19,19 +24,27 @@ export default function Home(props: any) {
         }))
     ]
 
+    /**
+     * click handle of github icon
+     */
     const onGithubLogoIconClick = () => {
-        window.open('https://github.com/Mumujianguang')
+        const github = MY_LINKS.find(item => item.name === 'github')
+        if (github) {
+            window.open(github.url, '_blank')
+        }
     }
 
     return (
         <div className={styles["mm-site-home"]}>
             {/* header */}
             <div className={styles['mm-site-home-header']}>
+                {/* header left */}
                 <div className={styles['mm-site-home-header-left']}>
                     <div className={styles['mm-site-home-header-avatar']}></div>
                     <Signature />
                 </div>
                 
+                {/* header menu */}
                 <Menu3D
                     menus={router.map(({ id, title, type, icon }) => ({
                         id,
@@ -45,11 +58,13 @@ export default function Home(props: any) {
                     }}
                 />
 
+                {/* header right */}
                 <div className={styles['mm-site-home-header-right']}>
                     <GitHubLogoIcon width={'24px'} height={'24px'} onClick={onGithubLogoIconClick} />
                 </div>
             </div>
 
+            {/* content */}
             <div className={styles['mm-site-home-content']}>
                 <EffectRouter
                     routers={routers}
