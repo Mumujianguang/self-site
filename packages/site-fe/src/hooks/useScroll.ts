48285ace7001484a2024e96ba88dda26f14df0d5
linjianguang <1419479392@qq.com>
2024-11-19 15:17:05 +0800

    feat(site-fe): add content progress for modal

--- packages/site-fe/src/hooks/useScroll.ts
+++ packages/site-fe/src/hooks/useScroll.ts
@@ -1,36 +1,50 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useState } from "react";
 
-export default function useScroll(target: HTMLElement) {
+export default function useScroll(target: RefObject<HTMLElement | null>) {
     const [scrollPercent, setScrollPercent] = useState(0)
 
     const getScrollPercent = () => {
-        const {  scrollTop, scrollHeight, clientHeight } = target
+        if (!target.current) {
+            return 0;
+        }
+        
+        const {  scrollTop, scrollHeight, clientHeight } = target.current
+        
+        if (scrollHeight <= clientHeight) {
+            return 0;
+        }
 
         const scrollPercent = Math.round((scrollTop / (scrollHeight - clientHeight)) * 100);
         return scrollPercent;
     };
 
     const scrollTo = (percent: number) => {
-        const scrollHeight = document.documentElement.scrollHeight;
+        if (!target.current) {  
+            return;
+        }
+
+        const scrollHeight = target.current.scrollHeight;
         const scrollTop = Math.round((scrollHeight * percent) / 100);
         
-        target.scrollTo({
+        target.current.scrollTo({
             top: scrollTop,
             behavior: 'smooth'
         })
     }
 
     useEffect(() => {
+        if (!target.current) {
+            return;
+        }
+
         const handleScroll = () => {
             const scrollPercent = getScrollPercent();
             setScrollPercent(scrollPercent);
         }
 
-        target.addEventListener('scroll', handleScroll);
-
-        return () => {
-            target.removeEventListener('scroll', handleScroll);
-        }
+        target.current.addEventListener('scroll', handleScroll);
+        return () => target.current?.removeEventListener('scroll', handleScroll);
+        
     }, [])
 
     return {
