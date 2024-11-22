cb3421f27de48392419010758c816f798504560d
linjianguang <1419479392@qq.com>
2024-11-22 11:10:55 +0800

    test(ci/cd): adjust script

--- packages/site-server/src/env.ts
+++ packages/site-server/src/env.ts
@@ -1,7 +1,8 @@
 import * as path from 'node:path';
 import * as dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+console.log(process.env.NODE_ENV)
+const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production';
 
 dotenv.config({
     path: path.resolve(__dirname, `../${envFile}`)
