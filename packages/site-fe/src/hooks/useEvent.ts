019b39f0ef0c4be9723d7e295402ebd39755d390
linjianguang <1419479392@qq.com>
2024-11-25 10:14:51 +0800

    chore(useEvent): add type constraints

--- packages/site-fe/src/hooks/useEvent.ts
+++ packages/site-fe/src/hooks/useEvent.ts
@@ -1,16 +1,20 @@
 import { useCallback, useRef } from "react";
 
+type IEventFunction<T extends (...args: any) => any> = T extends (...args: infer Args) => infer Result ? (...args: Args) => Result : never;
+
 /**
  * 创建一个 引用 不变的回调函数
  * @param callback
  * @returns
  */
-export default function useEvent(callback: Function) {
-    const ref = useRef<any>({});
+export default function useEvent<T extends (...args: any[]) => any>(callback: T) {
+    const ref = useRef<{ callback: IEventFunction<T> | null }>({
+        callback: null
+    });
 
-    ref.current.callback = callback
+    ref.current.callback = callback as unknown as IEventFunction<T>;
 
-    return useCallback((...args: any): any => {
-        return ref.current.callback.call(null, ...args)
+    return useCallback((...args: Parameters<IEventFunction<T>>): ReturnType<IEventFunction<T>> => {
+        return ref.current.callback?.call(null, ...args)
     }, [])
 }
