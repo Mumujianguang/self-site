24dafb268389b6d95d2c4c070d513165eb303ab5
linjianguang <1419479392@qq.com>
2025-03-31 00:19:56 +0800

    feat(fe): add resume

--- packages/site-fe/vite.config.ts
+++ packages/site-fe/vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 // import viteSsrPlugin from 'vite-ssr/plugin.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        {
+            enforce: 'pre',
+            ...mdx()
+        },
         react(),
-        // viteSsrPlugin()
     ],
 
     define: {
@@ -37,6 +41,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': '/src'
-        }
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.mdx', '.md']
     }
 })
